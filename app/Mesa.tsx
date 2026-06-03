cda5295816397a0f6c77c6a11b754db6690dc9c1
ohachii <wederpessoa202@gmail.com>
2026-06-03 20:43:37 -0300

    Mesa e CharacterCard

--- app/Mesa.tsx
+++ app/Mesa.tsx
@@ -1,10 +1,35 @@
-import { ProgressBar } from "@/components/campanha/Mesas/ProgressBar";
+import { CharacterCard } from "@/components/campanha/Mesas/CharacterCard";
+import PageHeader from "@/components/common/PageHeader";
 import ThemedView from "@/components/common/ThemedView";
+import { View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Mesa() {
     return (
-        <ThemedView style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-            <ProgressBar current={50} max={100} tipo="vida" label="Vida" />
-        </ThemedView>
-    )
+         <SafeAreaView style={styles.safeArea}>
+            {/* Falta implementar a lógica de selecionar a campanha e carregar os personagens dela, por enquanto só tem um exemplo estático */}
+        <ThemedView style={styles.container}>
+            <PageHeader title="Mesa" subtitle="Visualize sua ficha dentro da campanha." />
+             <View style={styles.characters}>
+           <CharacterCard nome="Aragorn" descricao="Guerreiro" avatar={require("@/assets/images/icon.png")} vidaAtual={50} vidaMax={100} />
+           <CharacterCard nome="Aragorn" descricao="Guerreiro" avatar={require("@/assets/images/icon.png")} vidaAtual={50} vidaMax={100} />
+           <CharacterCard nome="Aragorn" descricao="Guerreiro" avatar={require("@/assets/images/icon.png")} vidaAtual={50} vidaMax={100} />
+           </View>
+           </ThemedView>
+     </SafeAreaView>
+    );
+}
+
+const styles = {
+    safeArea: {
+        flex: 1
+    },
+    container: {
+        flex: 1,
+        padding: 24,
+        gap: 24
+    },
+    characters: {
+        gap: 20,
+    }
 }
---
+++ components/campanha/Mesas/CharacterCard.tsx
@@ -0,0 +1,75 @@
+import { StyleSheet, View } from "react-native";
+import Avatar from "@/components/common/Avatar";
+import ThemedText from "@/components/common/ThemedText";
+import { ImageType } from "@/types/Types";
+import { ProgressBar } from "@/components/common/ProgressBar";
+
+interface PropriedadesCharacterCard {
+  nome: string;
+  descricao: string; 
+  avatar: ImageType;
+  level?: number;
+  vidaAtual: number;
+  vidaMax: number;
+}
+
+export const CharacterCard = ({
+  nome,
+  descricao,
+  avatar,
+  vidaAtual,
+  vidaMax,
+}: PropriedadesCharacterCard) => {
+  return (
+    <View style={estilos.cardContainer}>
+      <Avatar avatar={avatar} />
+      
+      <View style={estilos.infoContainer}>
+        <ThemedText style={estilos.nome}>{nome}</ThemedText>
+        <View style={{ flexDirection: "row", justifyContent: "space-between"}}>
+          <ThemedText style={estilos.descricao}>{descricao}</ThemedText>
+          <ThemedText style={estilos.level}>Level {vidaMax / 10}</ThemedText>
+        </View>
+        <ProgressBar current={vidaAtual} max={vidaMax} tipo="vida" showText={false} />
+      </View>
+    </View>
+  );
+};
+
+const estilos = StyleSheet.create({
+  cardContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 16,
+    backgroundColor: "#171f2c", 
+    borderRadius: 12,
+    borderWidth: 1,
+    borderColor: "#2c3b52",
+    marginBottom: 16, 
+  },
+  infoContainer: {
+    flex: 1, 
+    marginLeft: 16,
+    justifyContent: "center",
+  },
+  nome: {
+    fontSize: 16,
+    color: "#e2e8f0", 
+    textTransform: "uppercase", 
+    letterSpacing: 1,
+    marginBottom: 4,
+    fontFamily: "serif", 
+  },
+  descricao: {
+    fontSize: 12,
+    color: "#a0a8b8", 
+    textTransform: "uppercase",
+    marginBottom: 8, 
+  },
+  level: {
+    fontSize: 12,
+    color: "#a0a8b8", 
+    textTransform: "uppercase",
+    marginBottom: 8, 
+  },
+});
